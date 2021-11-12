32c4f6b3244eca6a477db4e7a2900a1ab871a599
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-11-12 08:30:14 +0100

    Add placeholder and some height to description

--- src/editor/CreateIssue.tsx
+++ src/editor/CreateIssue.tsx
@@ -65,7 +65,8 @@ const CreateIssue: FC<Props> = ({ connection, screenshot, bugshot }) => {
         <label tw="block">
           <span tw="text-gray-700">Description</span>
           <textarea
-            tw="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            tw="h-80 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder="Issue description"
             {...register("description")}
           ></textarea>
         </label>
