3e936ab715b19e31aa52fa46a5d768c1d1d3dcdf
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-02-17 11:19:53 +0100

    Create release ready zip

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const { name, version } = require("./package.json");
 
 const isDevelopment = process.env.NODE_ENV === "development";
+
+const plugins = [
+  new CleanWebpackPlugin(),
+  new CopyPlugin({
+    patterns: [{ from: "static", to: ".", transform: (content, absoluteFrom) => {
+      if (absoluteFrom.endsWith("manifest.json")) {
+        const manifest = JSON.parse(content);
+        manifest.version = version;
+        return JSON.stringify(manifest, null, 2);
+      }
+      return content;
+    } }],
+  }),
+];
+
+if (!isDevelopment) {
+  plugins.push(
+    new ZipPlugin({
+      filename: `${name}-${version}.zip`,
+      path: path.join(__dirname, "dist"),
+    })
+  );
+}
 
 module.exports = {
   mode: isDevelopment ? "development" : "production",
@@ -32,14 +57,9 @@ module.exports = {
     alias: { react: require.resolve("react") },
     extensions: [".tsx", ".ts", ".js", ".css"],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: "static", to: "." }],
-    }),
-  ],
+  plugins: plugins,
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
 };
